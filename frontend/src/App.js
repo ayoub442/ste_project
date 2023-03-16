e7309d2a8c0d71edb225e998f8ffc1d59f973f1a
ayoub442 <121465038+ayoub442@users.noreply.github.com>
2023-03-16 21:51:33 +0100

    Part_1

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">STE</a>
+      </header>
+      <main>STE CATALOGUE PIÈCES D'AUTOMOBILES</main>
+    </div>
+  );
+}
+
+export default App;

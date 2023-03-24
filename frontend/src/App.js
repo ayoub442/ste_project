5a77ef48b94c16932b38ce08e9140ad140adbed3
ayoub442 <121465038+ayoub442@users.noreply.github.com>
2023-03-24 09:34:30 +0100

    c2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header className="App-header">
         <a href="/">STE</a>
       </header>
-      <main>STE CATALOGUE PIÈCES D'AUTOMOBILES</main>
+      <main>
+        <h1>STE CATALOGUE PIÈCES D'AUTOMOBILES</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,32 @@
+const data = {
+  products: [
+    {
+      name: ' name1',
+      cat: 'cat1',
+      image: '/images/img1.jpg',
+      price: 1,
+      countInStock: 10,
+      description: 'disc1',
+    },
+
+    {
+      name: ' name2',
+      cat: 'cat',
+      image: '/images/img2.jpg',
+      price: 1,
+      countInStock: 10,
+      description: 'disc1',
+    },
+
+    {
+      name: ' name1',
+      cat: 'cat1',
+      image: '/images/img3.jpg',
+      price: 1,
+      countInStock: 10,
+      description: 'disc1',
+    },
+  ],
+};
+
+export default data;
